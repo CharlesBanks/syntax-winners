910fbae78cbbbe72ad7ffbd4a11e84f6d4896b9b
Charles Banks <charleslb.1512@gmail.com>
2022-05-31 18:14:57 -0400

    added 9 to third array

--- src/groupproject01/Sum2DArray.java
+++ src/groupproject01/Sum2DArray.java
@@ -7,7 +7,7 @@ public class Sum2DArray {
 		 * Create a 2D array of integer values. Print the sum of all numbers.
 		 */
 		int sum = 0;
-		int [][] numbers = { {1,2,3}, {4,5,6}, {7,8} };
+		int [][] numbers = { {1,2,3}, {4,5,6}, {7,8,9} };
 		
 		for(int index=0; index<numbers.length; index++) {
 			for(int index2=0; index2<numbers[index].length ; index2++) {
